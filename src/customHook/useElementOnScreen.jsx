6c9a69320e4b3d7cff7ed785e28274d9a9335596
jaikrishnaverma-dev <jaikrishnaverma@gmail.com>
2023-03-18 11:00:09 +0530

    third

--- src/customHook/useElementOnScreen.jsx
+++ src/customHook/useElementOnScreen.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import  { useEffect, useRef, useState } from 'react'
 
 const useElementOnScreen = (options) => {
       const containerRef = useRef(null);
